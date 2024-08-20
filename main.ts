51db4ad60065768e73963f3cbf3a8fca24bed899
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-08-20 12:21:32 +0200

    Ban yadayadayadagoodbye

--- main.ts
+++ main.ts
@@ -6,15 +6,15 @@ const password = Deno.env.get("BOT_PASSWORD");
 const admins = Deno.env.get("BOT_ADMINS")?.split(",");
 if (!username || !password || !admins) {
   throw new Error(
-    "Specify BOT_USERNAME and BOT_PASSWORD and BOT_ADMINS in the .env"
+    "Specify BOT_USERNAME and BOT_PASSWORD and BOT_ADMINS in the .env",
   );
 }
 
-const bot = new RoarBot({ admins });
+const bot = new RoarBot({ admins, banned: ["yadayadayadagoodbye"] });
 bot.run(
   import("./commands/geminium.ts"),
   import("./commands/js.ts"),
   import("./commands/symbolrace.ts"),
-  import("./commands/whois.ts")
+  import("./commands/whois.ts"),
 );
 bot.login(username, password);
