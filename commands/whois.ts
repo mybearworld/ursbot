6a9491f201409f738e2f4030b7e7306bb8de73c5
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-08-20 12:22:05 +0200

    Use non-deprecated username instead of u

--- commands/whois.ts
+++ commands/whois.ts
@@ -5,7 +5,7 @@ export default (bot: RoarBot) => {
     args: [{ name: "username", type: "string", optional: true }],
     description: "Get information about a user.",
     fn: async (reply, [user], post) => {
-      const profile = await bot.user(user ?? post.u);
+      const profile = await bot.user(user ?? post.username);
       await reply(`
 ### ${profile._id}
 **Created**: ${profile.created ? new Date(profile.created * 1000) : "idk"}
